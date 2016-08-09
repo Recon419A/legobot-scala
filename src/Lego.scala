4211367c56cee2f61ff4fb37200bc938185d463d
drew <donald.bronson@here.com>
2016-08-09 16:27:13 -0500

    Refactoring to pull WeatherListener out of Lego.scala

--- src/Lego.scala
+++ src/Lego.scala
@@ -17,25 +17,3 @@ abstract class Lego (var children: List[Lego] = Nil) {
     }
   }
 }
-
-class WeatherListener extends Lego {
-  class ZipCode extends Lego {
-    override def self_listening_for(msg: Message): Boolean = {
-      msg.text.contains("90210")
-    }
-
-    override def get_own_response(msg: Message): List[Message] = {
-      List(new Message("The weather is sunny. It's California.", None))
-    }
-  }
-
-  override def self_listening_for(msg: Message): Boolean = {
-    msg.text.contains("!weather")
-  }
-
-  override def get_own_response(msg: Message): List[Message] = {
-    children = new ZipCode() :: children
-    List(new Message("Please enter a zipcode:", None))
-  }
-}
-
